91e0f72799476e093a5118ca8163f4c542f9e1aa
aqa510415008 <510415008@qq.com>
2018-09-29 17:59:42 +0800

    Optimized unit test

--- test/Ocelot.Security.AuthorizationToken.EntityFramework.Test/AuthorizationTokenSecurityPolicyTests.cs
+++ test/Ocelot.Security.AuthorizationToken.EntityFramework.Test/AuthorizationTokenSecurityPolicyTests.cs
@@ -11,13 +11,15 @@ using Microsoft.AspNetCore.Http;
 using System.Net.Http;
 using Ocelot.Request.Middleware;
 using Ocelot.Responses;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 
 namespace Ocelot.Security.AuthorizationToken.EntityFramework.Test
 {
     public class AuthorizationTokenSecurityPolicyTests
     {
         private readonly Mock<IAuthorizationTokenStorage> _storage;
-        private readonly Mock<IMemoryCache> _cache;
+        private readonly IMemoryCache _cache;
         private readonly Mock<ILogger<AuthorizationTokenSecurityPolicy>> _logger;
         private readonly AuthorizationTokenSecurityPolicy _securityPolicy;
         private readonly DownstreamContext _downstreamContext;
@@ -26,10 +28,10 @@ namespace Ocelot.Security.AuthorizationToken.EntityFramework.Test
 
         public AuthorizationTokenSecurityPolicyTests()
         {
+            _cache = new MemoryCache(Options.Create<MemoryCacheOptions>(new MemoryCacheOptions()));
             _storage = new Mock<IAuthorizationTokenStorage>();
-            _cache = new Mock<IMemoryCache>();
             _logger = new Mock<ILogger<AuthorizationTokenSecurityPolicy>>();
-            _securityPolicy = new AuthorizationTokenSecurityPolicy(_cache.Object, _storage.Object, _logger.Object);
+            _securityPolicy = new AuthorizationTokenSecurityPolicy(_cache, _storage.Object, _logger.Object);
             _downstreamContext = new DownstreamContext(new DefaultHttpContext());
             _downstreamContext.DownstreamRequest = new DownstreamRequest(new HttpRequestMessage(HttpMethod.Get, "http://test.com"));
             _downstreamContext.HttpContext.Request.Headers.Add("Authorization", toekn);
@@ -47,38 +49,76 @@ namespace Ocelot.Security.AuthorizationToken.EntityFramework.Test
         [Fact]
         public void should_token_not_existe_blacklist()
         {
-            this.Given(x => x.GivenGetCacheNotExisteBlackList())
+            this
                 .When(x => x.WhenSecurity())
                 .Then(f => f.ThenSecurityPassing())
                 .BDDfy();
         }
 
+        [Fact]
+        public void should_token_blacklis_disappear()
+        {
+            this.Given(x => x.GivenStorage())
+                .When(x => x.WhenSecurityWait(5000))
+                .Then(x => x.ThenTokenBlacklisIsDisappear())
+                .BDDfy();
+        }
 
         private void GivenGetCacheExisteBlackList()
         {
-            object o="exist_token";
-            _cache.Setup(x => x.TryGetValue(toekn,out o)).Returns(true);
+            _cache.Set(toekn, "exist_token", new MemoryCacheEntryOptions()
+            {
+                AbsoluteExpiration = DateTime.Now.AddHours(1)
+            });
         }
-        private void GivenGetCacheNotExisteBlackList()
+     
+        private void GivenStorage()
         {
-            object result;
-            _cache.Setup(x => x.TryGetValue(toekn, out result)).Returns(false);
-        }
+            List<AuthorizationToken> tokens = new List<AuthorizationToken>();
+            tokens.Add(new AuthorizationToken()
+            {
+                AddTime = DateTime.Now,
+                Expiration = DateTime.Now.AddMilliseconds(1000),
+                Id = 1,
+                Token = "1",
+                WarnInfo = "123"
+            });
+            tokens.Add(new AuthorizationToken()
+            {
+                AddTime = DateTime.Now,
+                Expiration = DateTime.Now.AddHours(1),
+                Id = 1,
+                Token = "2",
+                WarnInfo = "123"
+            });
+            _storage.Setup(x => x.GetList(0)).Returns(Task.FromResult(tokens));
 
+        }
         private void WhenSecurity()
         {
             response = _securityPolicy.Security(_downstreamContext).GetAwaiter().GetResult();
+        }
+
+        private void WhenSecurityWait(int millisecondsDelay)
+        {
+            response = _securityPolicy.Security(_downstreamContext).GetAwaiter().GetResult();
+            Task.Delay(millisecondsDelay).Wait();
         }
 
         private void ThenSecurityPassing()
         {
             Assert.False(response.IsError);
         }
-
         private void ThenSecurityNotPassing()
         {
             Assert.Equal(401, _downstreamContext.HttpContext.Response.StatusCode);
             Assert.True(response.IsError);
+        }
+        private void ThenTokenBlacklisIsDisappear()
+        {
+            Assert.False(_cache.TryGetValue("1", out string a));
+            Assert.True(_cache.TryGetValue("2", out string b));
+            Assert.False(response.IsError);
         }
     }
 }
