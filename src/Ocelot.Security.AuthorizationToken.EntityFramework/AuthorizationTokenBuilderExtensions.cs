69fc35715b6563db85a45b4a38b9989e8e582a3d
aqa510415008 <510415008@qq.com>
2018-12-05 18:33:18 +0800

    optimization configuration

---
+++ src/Ocelot.Security.AuthorizationToken.EntityFramework/AuthorizationTokenBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.Security;
+using Ocelot.Security.AuthorizationToken;
+using Ocelot.Security.AuthorizationToken.Storage;
+using System;
+
+namespace Ocelot.DependencyInjection
+{
+    public static class AuthorizationTokenBuilderExtensions
+    {
+        public static IOcelotBuilder AddSecurityAuthorizationToken<TDbContent>(this IOcelotBuilder builder,Action<DbContextOptionsBuilder> optionsAction)
+            where TDbContent:DbContext 
+        {
+            builder.Services.AddSingleton<IAuthorizationTokenStorage, AuthorizationTokenStorage>();
+            builder.Services.AddSingleton<ISecurityPolicy, AuthorizationTokenSecurityPolicy>();
+            builder.Services.AddDbContext<OcelotSecurityStorageDbContent>(optionsAction);
+            return builder;
+        }
+
+    }
+}
